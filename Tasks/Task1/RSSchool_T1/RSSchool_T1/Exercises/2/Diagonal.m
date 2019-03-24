1fc14868aa0f796596c8720ff6e82c0e8a1ec66e
Denis <denis360_93@mail.ru>
2019-03-24 19:36:28 +0300

    complete diagonal task

--- Tasks/Task1/RSSchool_T1/RSSchool_T1/Exercises/2/Diagonal.m
+++ Tasks/Task1/RSSchool_T1/RSSchool_T1/Exercises/2/Diagonal.m
@@ -4,7 +4,31 @@
 
 // Complete the diagonalDifference function below.
 - (NSNumber *) diagonalDifference:(NSArray *)array {
-    return 0;
+    NSMutableArray* matrix = [NSMutableArray new];
+    NSInteger firstDiagonal = 0;
+    NSInteger secondDiagonal = 0;
+
+    for (NSString* value in array) {
+        NSArray* values = [value componentsSeparatedByString:@" "];
+        [matrix addObject:values];
+    }
+
+    for (int i = 0; i < [matrix count]; i++) {
+        NSArray* subMatrix = [matrix objectAtIndex:i];
+        for (int j = 0; j < [subMatrix count]; j++) {
+            if (i == j) {
+                firstDiagonal += [[subMatrix objectAtIndex: j] intValue];
+                secondDiagonal += [[subMatrix objectAtIndex: [subMatrix count] - 1 - j] intValue];
+            }
+        }
+        [subMatrix release];
+    }
+
+    if (firstDiagonal > secondDiagonal) {
+        return [@(firstDiagonal - secondDiagonal) autorelease];
+    } else {
+        return [@(secondDiagonal - firstDiagonal) autorelease];
+    }
 }
 
 @end
