6fcb320a42fecf44a96a566ebd11d3143e924273
Denis <denis360_93@mail.ru>
2019-03-24 19:36:55 +0300

    complete encryption task

--- Tasks/Task1/RSSchool_T1/RSSchool_T1/Exercises/4/Encryption.m
+++ Tasks/Task1/RSSchool_T1/RSSchool_T1/Exercises/4/Encryption.m
@@ -4,7 +4,45 @@
 
 // Complete the encryption function below.
 - (NSString *)encryption:(NSString *)string {
-    return @"";
+    NSString* pure = [string stringByReplacingOccurrencesOfString:@" " withString:@""];
+    NSInteger count = [pure length];
+    NSInteger column = (NSInteger)ceil(sqrt(count));
+    NSInteger row = (NSInteger)ceil(count / (float)column);
+
+    
+    NSMutableArray *array = [NSMutableArray array];
+    for (int i = 0; i < [pure length]; i++) {
+        NSString *ch = [pure substringWithRange:NSMakeRange(i, 1)];
+        [array addObject:ch];
+        [ch release];
+    }
+    
+    NSMutableArray *encrptions = [NSMutableArray array];
+    for (int i = 0; i < row; i++) {
+        NSMutableArray* result = [NSMutableArray new];
+        for (int j = 0; j < column; j++) {
+            if ([array count] > column*i+j) {
+                [result addObject:[array objectAtIndex:column*i+j]];
+            }
+        }
+        [encrptions addObject:result];
+        [result release];
+    }
+    
+    NSString* resultStr = @"";
+    for (int i = 0; i < column; i++) {
+        for (int j = 0; j < row; j++) {
+            if ([[encrptions objectAtIndex:j] count] > i) {
+                resultStr = [resultStr stringByAppendingString:[[encrptions objectAtIndex:j] objectAtIndex:i]];
+            }
+        }
+        if (i != column - 1) {
+            resultStr = [resultStr stringByAppendingString:@" "];
+        }
+    }
+
+    [pure release];
+    return resultStr;
 }
 
 @end
